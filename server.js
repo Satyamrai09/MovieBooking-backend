e95febb2d9d799fef4adb7fee19d82a76c4e0df5
Satyam Rai <satyamrai0911@gmail.com>
2022-05-13 05:10:52 +0530

    Checkpoint 6 Part B

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-
 const express = require("express");
-const cors = require("cors");
 const app = express();
-const port = 9000;
-app.use(cors());
+
+const db = require("./Movie-Booking-App-Backend-Code/moviebooking/models");
+const PORT = 3000;
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
+const router = require("express").Router();
+
+const artistRouter = require("./Movie-Booking-App-Backend-Code/moviebooking/routes/artist.routes")(router);
+const genreRouter = require("./Movie-Booking-App-Backend-Code/moviebooking/routes/genre.routes")(router);
+const movieRouter = require("./Movie-Booking-App-Backend-Code/moviebooking/routes/movie.routes.js")(router);
+
+app.use("/api", movieRouter, genreRouter, artistRouter);
 
 app.get("/", (req, res) => {
   res.json({
@@ -11,6 +23,18 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(port, () => {
-    console.log('Connection Established on PORT ',port);
-}); 
+app.listen(PORT, () => {
+  console.log("Connection Established on PORT ", PORT);
+});
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  }); 
