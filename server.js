0c2ea1f07032bff19f60c8b24e4ef0b91649f2b9
Satyam Rai <satyamrai0911@gmail.com>
2022-05-13 04:19:50 +0530

    Checkpoint 4

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ const server = http.createServer((req,res)=>{
     }
 })
 
-server.listen(3000);
-console.log('ServerListening on port 3000...'); 
+server.listen(9000);
+console.log('ServerListening on port 9000...'); 
