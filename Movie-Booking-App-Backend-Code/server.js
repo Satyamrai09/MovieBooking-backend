cfbc550ed2700152b7a57dec1617138b72628c4e
Satyam Rai <satyamrai0911@gmail.com>
2022-05-13 03:16:53 +0530

    Checkpoint 2

---
+++ Movie-Booking-App-Backend-Code/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    if(req.url === '/movies'){
+        res.write('All Movies Data in JSON format from Mongo DB');
+        res.end();
+    }else if(req.url === '/genres'){
+        res.write('All Geners Data in JSON format from Mongo DB');
+        res.end();
+    }else if(req.url === '/artists'){
+        res.write('All Artists Data in JSON format from Mongo DB');
+        res.end();
+
+    }
+})
+
+server.listen(3000);
+console.log('ServerListening on port 3000...'); 
